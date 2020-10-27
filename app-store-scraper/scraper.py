c9960f7922bf9bcc8bb1cb8ddf60c21af9905d08
Siddharth Venu <siddharthvvenu@gmail.com>
2020-10-27 19:19:54 +0530

    feat: build app store scraper

---
+++ app-store-scraper/scraper.py
@@ -0,0 +1,22 @@
+from app_store_scraper import AppStore
+import json
+
+
+def normalize_review(review: dict):
+    review["text"] = review.pop("review")
+    review["score"] = review.pop("rating")
+    review["date"] = review["date"].timestamp()
+    return review
+
+
+def scrape_data():
+    app_info = AppStore(app_name="WhiteHat Jr: Online Coding", country="in")
+    app_info.review(how_many=3000)
+    reviews = list(map(lambda review: normalize_review(review), app_info.reviews))
+    f = open("../app-store-reviews.json", "w")
+    f.write(json.dumps(reviews))
+    f.close()
+
+
+if __name__ == '__main__':
+    scrape_data()
