1adbfbf3a8c0e60fd386b43e39e6f119b33e5bad
Siddharth Venu <siddharthvvenu@gmail.com>
2020-10-27 20:50:06 +0530

    feat: modify analyser algos to be generic, edit output file names, run algo on app store reviews

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -9,9 +9,15 @@ from typing import List
 similarity_threshold = 0.8
 vectorizer = TfidfVectorizer()
 
+reviews_file_name = input("Input the path to reviews file: ")
+if reviews_file_name.endswith(".json"):
+    reviews_file_name = reviews_file_name[:-5]
+
 
 def analyze():
-    reviews = json.loads(open("../reviews.json", "r").read())["data"]
+    global reviews_file_name
+
+    reviews = json.loads(open(f"{reviews_file_name}.json", "r").read())
     """
     id - Identifier of a review
     text - Review Text
@@ -56,7 +62,7 @@ def analyze():
         if not is_matched:
             groups.append(reviews[i])
             cur_idx += 1
-    f = open("groups.json", "w")
+    f = open(f"{reviews_file_name}-groups.json", "w")
     f.write(json.dumps(groups))
     f.close()
 
@@ -66,15 +72,18 @@ def clean_review_groups(review_groups: List[list]):
 
 
 def infer():
-    groups = json.loads(open("groups.json", "r").read())
+    groups = json.loads(open(f"{reviews_file_name}-groups.json", "r").read())
     low_probability_fakes = list(filter(lambda g: len(g) > 20, groups))
-    high_probability_fakes = list(filter(lambda g: len(g) > 1 and len(re.findall(r'\w+', g[0]["text"])) > 9, groups))
-    f = open("low_probability_fakes.json", "w")
+    high_probability_fakes = list(filter(
+        lambda g: type(g) == list and len(g) > 1 and len(re.findall(r'\w+', g[0]["text"])) > 9,
+        groups))
+    f = open(f"{reviews_file_name}-low-probability-fakes.json", "w")
     f.write(json.dumps(clean_review_groups(low_probability_fakes), indent=2))
     f.close()
-    f = open("high_probability_fakes.json", "w")
+    f = open(f"{reviews_file_name}-high-probability-fakes.json", "w")
     f.write(json.dumps(clean_review_groups(high_probability_fakes), indent=2))
     f.close()
 
 
+analyze()
 infer()
